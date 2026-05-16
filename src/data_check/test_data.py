1491f3afa2538dc7659283b1fbb5617bf2688493
Shivam Sharma <shivamsharma@Shivams-MacBook-Air.local>
2026-05-16 11:46:23 +0530

    Added code to perform full pipeline for production release

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -63,3 +63,17 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 ########################################################
 # Implement here test_row_count and test_price_range   #
 ########################################################
+
+def test_row_count(data):
+    """
+    Test the number of rows is reasonable (not too many, not too few)
+    """
+    assert 15000 < data.shape[0] < 1000000
+
+
+def test_price_range(data, min_price, max_price):
+    """
+    Test that the price values are between min_price and max_price
+    """
+    assert data['price'].between(min_price, max_price).all()
+    
